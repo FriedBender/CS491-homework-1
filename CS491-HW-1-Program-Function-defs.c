c3a2ff2cf5da27d657ebc2406ba17fe27789d189
semchuk2 <semchuk2@pdx.edu>
2021-04-05 16:49:12 -0700

    question #2 is answered

---
+++ CS491-HW-1-Program-Function-defs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "CS491-HW-1-Program-Function-defs.h"
+
+
+int get_letter_frequency(char* a_string){
+    printf("Here is the file:\n%s", a_string);
+    return 0;
+}
---
+++ CS491-HW-1-Program-Function-defs.h
@@ -0,0 +1,11 @@
+#ifndef DEFS_HW_ONE
+#define DEFS_HW_ONE
+
+
+
+//This will get the frequency of each letter
+int get_letter_frequency(char* a_string);
+
+
+
+#endif
